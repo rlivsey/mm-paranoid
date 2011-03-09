7cc4682a227d48379c5408cbf25d043212ca5127
Richard Livsey <richard@livsey.org>
2011-03-09 16:40:24 +0000

    update to support mongo_mapper 0.9.0 - extend ActiveSupport::Concern etc...

--- lib/mm-paranoid.rb
+++ lib/mm-paranoid.rb
@@ -3,21 +3,18 @@ require 'mongo_mapper'
 module MongoMapper
   module Plugins
     module Paranoid
-      def self.included(model)
-        model.plugin self
-      end
+      extend ActiveSupport::Concern
 
-      def self.configure(base)
-        base.key(:deleted_at, Time, :index => true)
+      included do
+        key :deleted_at, Time, :index => true
       end
 
       module InstanceMethods
         def destroy
-          run_callbacks(:before_destroy)
-          self.deleted_at = Time.now
-          result = self.save
-          run_callbacks(:after_destroy)
-          result
+          run_callbacks(:destroy) do
+            self.deleted_at = Time.now
+            self.save
+          end
         end
 
         def deleted?
